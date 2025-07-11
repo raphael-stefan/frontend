6652e0b8196deaa50e83fd018b6fe96fe999a33d
raphael-stefan <57644078+raphael-stefan@users.noreply.github.com>
2025-07-11 00:52:17 -0700

    mais alteracoes feitas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
+import { About } from './components/about/about';
+
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: '**', redirectTo: '', pathMatch: 'full' }
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+  { path: 'about', component: About },
+
 ];
