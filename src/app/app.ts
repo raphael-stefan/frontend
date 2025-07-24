16143d193e82604ec259d02cb40f7e64c3d217c8
Marcelo Roner <marceloroner90@gmail.com>
2025-07-24 01:15:18 -0300

    haha

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { Hero } from './components/hero/hero';
-import { About } from './components/about/about';
-import { Gallery } from './components/gallery/gallery';
-import { Services } from './components/services/services';
-import { ContactComponent } from './components/contact/contact';
-import { Footer } from './components/footer/footer';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterModule,
-    Hero,
-    About,
-    Gallery,
-    Services,
-    ContactComponent,
-    Footer,
     RouterOutlet
   ],
   templateUrl: './app.html',
